c3d866d853450a4c7b9987b90e16248d42c2ef7b
Johan <sbkg0002@gmail.com>
2015-12-12 11:40:24 +0100

    first sync

---
+++ AndroidTVappTutorial/app/src/main/java/com/zao/androidtvapptutorial/CardPresenter.java
@@ -0,0 +1,88 @@
+package com.zao.androidtvapptutorial;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v17.leanback.widget.ImageCardView;
+import android.support.v17.leanback.widget.Presenter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Modified from AOSP sample source code, by corochann on 2/7/2015.
+ */
+public class CardPresenter extends Presenter {
+
+    private static final String TAG = CardPresenter.class.getSimpleName();
+
+    private static Context mContext;
+    private static int CARD_WIDTH = 313;
+    private static int CARD_HEIGHT = 176;
+
+    static class ViewHolder extends Presenter.ViewHolder {
+        private Movie mMovie;
+        private ImageCardView mCardView;
+        private Drawable mDefaultCardImage;
+
+        public ViewHolder(View view) {
+            super(view);
+            mCardView = (ImageCardView) view;
+            mDefaultCardImage = mContext.getResources().getDrawable(R.drawable.movie);
+        }
+
+        public void setMovie(Movie m) {
+            mMovie = m;
+        }
+
+        public Movie getMovie() {
+            return mMovie;
+        }
+
+        public ImageCardView getCardView() {
+            return mCardView;
+        }
+
+        public Drawable getDefaultCardImage() {
+            return mDefaultCardImage;
+        }
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent) {
+        Log.d(TAG, "onCreateViewHolder");
+        mContext = parent.getContext();
+
+        ImageCardView cardView = new ImageCardView(mContext);
+        cardView.setFocusable(true);
+        cardView.setFocusableInTouchMode(true);
+        cardView.setBackgroundColor(mContext.getResources().getColor(R.color.fastlane_background));
+        return new ViewHolder(cardView);
+    }
+
+    @Override
+    public void onBindViewHolder(Presenter.ViewHolder viewHolder, Object item) {
+        Movie movie = (Movie) item;
+        ((ViewHolder) viewHolder).setMovie(movie);
+
+        Log.d(TAG, "onBindViewHolder");
+        ((ViewHolder) viewHolder).mCardView.setTitleText(movie.getTitle());
+        ((ViewHolder) viewHolder).mCardView.setContentText(movie.getStudio());
+        ((ViewHolder) viewHolder).mCardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT);
+        ((ViewHolder) viewHolder).mCardView.setMainImage(((ViewHolder) viewHolder).getDefaultCardImage());
+    }
+
+    @Override
+    public void onUnbindViewHolder(Presenter.ViewHolder viewHolder) {
+        Log.d(TAG, "onUnbindViewHolder");
+    }
+
+    @Override
+    public void onViewAttachedToWindow(Presenter.ViewHolder viewHolder) {
+        // TO DO
+    }
+
+}
---
+++ AndroidTVappTutorial/app/src/main/java/com/zao/androidtvapptutorial/PlaybackOverlayActivity.java
@@ -0,0 +1,14 @@
+package com.zao.androidtvapptutorial;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class PlaybackOverlayActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playback_overlay);
+    }
+
+}
